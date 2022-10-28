c26b11d298ac47a134c9397d940351f505a2c7c3
thiago <thiagoabreus@outlook.com>
2022-10-28 14:49:01 -0300

    improve to v2

--- turing-game-card-challenge.py
+++ turing-game-card-challenge.py
@@ -1,34 +1,29 @@
 def maxCard(cards):
+    
+    visited = []
     lst = []
-    duplicate = []
-    maxCard = float("-inf")
+
     for i in range(0, len(cards)):
-        _cards = cards[i]
-        _cards.sort(reverse=True)
-        if len(_cards) == 1 and _cards[0] > maxCard:
-            lst.append(_cards[0])
-            maxCard = _cards[0]
-        for j in range(1, len(_cards)):
-            card = _cards[j]
-            previews_card = _cards[j-1]
-            
-            if card == previews_card:
-                duplicate.append(card)
-            
-            if previews_card > maxCard and previews_card not in duplicate:
-                lst.append(previews_card)
-                maxCard = previews_card
-           
+        current_cards = cards[i]
+        current_cards.sort(reverse=True)
 
+        low = 0
+        high = len(current_cards)
+
+        while low < high:
+            if current_cards[low] in current_cards[low+1:high]:
+                visited.append(current_cards[low])
+            low += 1
+    
+        for card in current_cards:
+            if card not in visited:
+                lst.append(card)
+ 
     return -1 if len(lst) == 0 else max(lst)
-        
-cards = [
-    [5, 7, 3, 9, 4, 9, 8, 3, 1], 
-    [1, 2, 2, 4, 4, 1], 
-    [1, 2, 3]
-]
 
-cards = [[5, 3, 6, 6, 6], [9, 9]]
+
+cards = [[11, 2, 3], [3, 3, 3, 4, 4, 6, 7]]
+
 
 mc = maxCard(cards)
 print(mc)
